883733502af301acee47e8b089915833afebc71f
trevtanner <trevtanner@gmail.com>
2021-09-21 17:40:31 -0700

    Fixed adding transaction coin amounts

--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState } from "react";
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
-import Heading from './components/UI/Heading'
-import Footer from './components/UI/Layout/Footer';
-import Totals from './components/Totals/Totals';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+import Heading from "./components/UI/Heading";
+import Footer from "./components/UI/Layout/Footer";
+import Totals from "./components/Totals/Totals";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   const [transactionsList, setTransactionsList] = useState([]);
+  const [btcTotal, setBtcTotal] = useState(0);
+  const [ethTotal, setEthTotal] = useState(0);
+  const [adaTotal, setAdaTotal] = useState(0);
+  const [bscTotal, setBscTotal] = useState(0);
 
   const addUserHandler = (transaction) => {
     setTransactionsList((prevTransactionsList) => {
-      return [
-       ...prevTransactionsList, transaction
-      ];
+      return [...prevTransactionsList, transaction];
     });
+
+    //fix this, current returns a string, so new amounts are appended to end of string intead of added
+    //FIXED! Just needed to parse them as floats
+    const addToTotal = () => {
+      if (transaction.coin === "BTC") {
+        setBtcTotal(prevState => parseFloat(prevState) + parseFloat(transaction.amount));
+      }
+      if (transaction.coin === "ETH") {
+        setEthTotal(ethTotal + transaction.amount);
+      }
+      if (transaction.coin === "ADA") {
+        setAdaTotal(adaTotal + transaction.amount);
+      }
+      if (transaction.coin === "BSC") {
+        setBscTotal(bscTotal + transaction.amount);
+      }
+    };
+    addToTotal(parseInt(transaction));
   };
 
   return (
     <Fragment>
-    <Heading />
+      <Heading />
       <AddUser onSaveTransactionData={addUserHandler} />
-      <Totals transactions={transactionsList} />
+      <Totals
+        transactions={transactionsList}
+        btcTotal={btcTotal}
+        ethTotal={ethTotal}
+        adaTotal={adaTotal}
+        bscTotal={bscTotal}
+      />
       <UsersList transactions={transactionsList} />
-    <Footer />
+      <Footer />
     </Fragment>
   );
-}
+};
 
 export default App;
--- src/components/Totals/Totals.js
+++ src/components/Totals/Totals.js
@@ -1,31 +1,17 @@
-import React, { useState } from "react";
-
 import Card from "../UI/Card"
 import classes from "../Users/UsersList.module.css";
 
 
 
 const Totals = (props) => {
-    let btcTotal = 0;
-    let ethTotal = 0;
-    let adaTotal = 0;
-    let bscTotal = 0;
-
-    if (props.coin === 'BTC') {
-        btcTotal += props.amount;
-        return btcTotal
-    } if (props.coin === 'ETH') {
-        ethTotal += props.amount
-    }
-
 
     return (
             <Card className={classes.users}>
             <ul>
-                <li>BTC total: {btcTotal}</li>
-                <li>ETH total: {ethTotal}</li>
-                <li>ADA total:</li>
-                <li>BSC total:</li>
+                <li>BTC total: {props.btcTotal}</li>
+                <li>ETH total: {props.ethTotal}</li>
+                <li>ADA total: {props.adaTotal}</li>
+                <li>BSC total: {props.bscTotal}</li>
 
             </ul>
             </Card>
