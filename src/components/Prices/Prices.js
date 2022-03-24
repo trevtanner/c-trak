7aedc8d74a6f30cf067ba2f3ffb67c896521555e
Trevor Tanner <trevor.tanner.developer@gmail.com>
2022-03-24 14:51:33 -0700

    Updates

--- src/components/Prices/Prices.js
+++ src/components/Prices/Prices.js
@@ -1,24 +1,55 @@
 import { useState, useEffect } from "react";
+import { Alert, CircularProgress, Grid } from "@mui/material";
 import FadeIn from "react-fade-in";
 import { Container, Card } from "semantic-ui-react";
+import { computeHeadingLevel } from "@testing-library/react";
 
 const Prices = () => {
-  const [data, setData] = useState([]);
+  const [error, setError] = useState(undefined);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
   const [coinId, setCoinId] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
-      await fetch(
-        `${process.env.REACT_APP_API_URL}/ticker?key=${process.env.REACT_APP_API_KEY}&ids=BTC,ETH,ADA`
-      )
-        .then((res) => res.json())
-        .then((result) => {
-          setData(result);
-          console.log(result);
-        });
+      try {
+        const response = await fetch(
+          'https://api.binance.com/api/v3/avgPrice'
+        );
+        const data = await response.json();
+        setCoins(data.results);
+      } catch (e) {
+        setError(e.message || "Something went wrong");
+      }
+
+      setLoading(false);
     };
     fetchData();
-  }, [data, coinId]);
+    console.log(coins);
+  }, []);
+
+  if (error) {
+    return <Alert severity="error">{error}</Alert>;
+  }
+
+  if (loading) {
+    return (
+      <Grid container justify="center">
+        <CircularProgress />
+      </Grid>
+    );
+  }
+
+  // const coinsList = coins.map((coin) => (
+  //   <Card>
+  //   <Card.Content>
+  //     <Card.Header>{coin.name}</Card.Header>
+  //     <Card.Meta>BTC</Card.Meta>
+  //     <Card.Description>$ Current Price</Card.Description>
+  //   </Card.Content>
+  // </Card>
+  // ))
+
   return (
     <Container className="pt-4 pb-4">
       <FadeIn>
--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,99 +1,164 @@
-import { Fragment } from "react"
-import { Container, Card } from "semantic-ui-react";
+import { Fragment } from "react";
 import FadeIn from "react-fade-in";
-import  classes  from './Resources.module.css'
+import classes from "./Resources.module.css";
+import {
+  CardActions,
+  Typography,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Divider,
+} from "@mui/material";
 
 const Resources = () => {
-    return (
-        <Container className="pt-4 pb-4">
-            <FadeIn>
-            <div className={classes.exchanges}>
+  const exchanges = [
+    {
+      name: "Binance.US",
+      url: "https://www.binance.us",
+      description: "CEX",
+    },
+    {
+      name: "Gemini",
+      url: "https://www.gemini.com",
+      description: "CEX",
+    },
+    {
+      name: "Crypto.com",
+      url: "https://www.crypto.com",
+      description: "CEX",
+    },
+  ];
+  const prices = [
+    {
+      name: "CoinGecko",
+      url: "https://www.coingecko.com",
+      description: "Price info",
+    },
+    {
+      name: "Coin Market Cap",
+      url: "https://www.coinmarketcap.com",
+      description: "Price Info",
+    },
+    {
+      name: "CoinStats",
+      url: "https://www.coinstats.com",
+      description: "Price Info / Portfolio",
+    },
+  ];
+  const social = [
+    {
+      name: "Lunar Crush",
+      url: "https://www.lunarcrush.com",
+      description: "Crypto social data",
+    },
+    {
+      name: "Twitter",
+      url: "https://www.twitter.com",
+      description: "Twitter",
+    },
+    {
+      name: "r/Cryptocurrency",
+      url: "https://www.reddit.com/r/cryptocurrency",
+      description: "Crypto subreddit",
+    },
+  ];
+
+  const exchangeInfo = exchanges.map((entry) => (
+    <Grid item xs={4}>
+      <Card sx={{ maxWidth: "200px", maxHeight: "300px", margin: 'auto', textAlign: 'center'  }}>
+        <CardContent
+          sx={{
+            margin: "auto",
+            alignItems: "center",
+          }}
+        >
+          <Typography sx={{ fontSize: "14pt" }}>{entry.name}</Typography>
+          <Typography
+            variant={"caption"}
+            color="text.secondary"
+            component="div"
+          >
+            {entry.url}
+          </Typography>
+          <Divider className={classes.divider} light />
+          <Typography variant="body1">{entry.description}</Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  ));
+
+  const pricesInfo = prices.map((entry) => (
+    <Grid item xs={4}>
+      <Card sx={{ maxWidth: "200px", maxHeight: "300px", margin: 'auto', textAlign: 'center'  }}>
+        <CardContent
+          sx={{
+            margin: "auto",
+            alignItems: "center",
+          }}
+        >
+          <Typography sx={{ fontSize: "14pt" }}>{entry.name}</Typography>
+          <Typography
+            variant={"caption"}
+            color="text.secondary"
+            component="div"
+          >
+            {entry.url}
+          </Typography>
+          <Divider className={classes.divider} light />
+          <Typography variant="body1">{entry.description}</Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  ));
+
+  const socialInfo = social.map((entry) => (
+    <Grid item xs={4}>
+      <Card sx={{ maxWidth: "200px", maxHeight: "300px", margin: 'auto', textAlign: 'center'  }}>
+        <CardContent
+          sx={{
+            margin: "auto",
+            alignItems: "center",
+          }}
+        >
+          <Typography sx={{ fontSize: "14pt" }}>{entry.name}</Typography>
+          <Typography
+            variant={"caption"}
+            color="text.secondary"
+            component="div"
+          >
+            {entry.url}
+          </Typography>
+          <Divider className={classes.divider} light />
+          <Typography variant="body1">{entry.description}</Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  ));
+
+  return (
+    <FadeIn>
+      <Container>
+        <Grid container spacing={2} className={classes.exchanges}>
+          <Grid item xs={12}>
             <h1>Exchanges</h1>
-        <Card.Group itemsPerRow={3}>
-          <Card>
-            <Card.Content>
-              <Card.Header className={classes.heading}>Binance</Card.Header>
-              <Card.Meta>Place url here</Card.Meta>
-              <Card.Description>Basic description of site</Card.Description>
-            </Card.Content>
-          </Card>
-
-          <Card>
-            <Card.Content>
-              <Card.Header>Gemini</Card.Header>
-              <Card.Meta>Place url here</Card.Meta>
-              <Card.Description>Basic description of site</Card.Description>
-            </Card.Content>
-          </Card>
-
-          <Card>
-            <Card.Content>
-              <Card.Header>Crypto.com</Card.Header>
-              <Card.Meta>Place url here</Card.Meta>
-              <Card.Description>Basic description of site</Card.Description>
-            </Card.Content>
-          </Card>
-          </Card.Group>
-            </div>
-            <div className={classes.exchanges}>
+            <Grid container>
+              {exchangeInfo}
+            </Grid>
+          </Grid>
+          <Grid item xs={12} className={classes.exchanges}>
             <h1>Price Information</h1>
-            <Card.Group itemsPerRow={3}>
-          <Card>
-            <Card.Content>
-              <Card.Header>CoinGecko</Card.Header>
-              <Card.Meta>Place url here</Card.Meta>
-              <Card.Description>Basic description of site</Card.Description>
-            </Card.Content>
-          </Card>
-
-          <Card>
-            <Card.Content>
-              <Card.Header>CoinMarketCap</Card.Header>
-              <Card.Meta>Place url here</Card.Meta>
-              <Card.Description>Basic description of site</Card.Description>
-            </Card.Content>
-          </Card>
-
-          <Card>
-            <Card.Content>
-              <Card.Header>CoinStats</Card.Header>
-              <Card.Meta>Place url here</Card.Meta>
-              <Card.Description>Basic description of site</Card.Description>
-            </Card.Content>
-          </Card>
-          </Card.Group>
-            </div>
-            <div className={classes.exchanges}>
+            <Grid container>{pricesInfo}</Grid>
+          </Grid>
+          <Grid item xs={12} className={classes.exchanges}>
             <h1>Social</h1>
-            <Card.Group itemsPerRow={3}>
-          <Card>
-            <Card.Content>
-              <Card.Header>LunarCrush</Card.Header>
-              <Card.Meta>Place url here</Card.Meta>
-              <Card.Description>Basic description of site</Card.Description>
-            </Card.Content>
-          </Card>
-
-          <Card>
-            <Card.Content>
-              <Card.Header>Twitter</Card.Header>
-              <Card.Meta>Place url here</Card.Meta>
-              <Card.Description>Basic description of site</Card.Description>
-            </Card.Content>
-          </Card>
-
-          <Card>
-            <Card.Content>
-              <Card.Header>Cryptocurrency Subreddit</Card.Header>
-              <Card.Meta>Place url here</Card.Meta>
-              <Card.Description>Basic description of site</Card.Description>
-            </Card.Content>
-          </Card>
-          </Card.Group>
-            </div>
-            </FadeIn>
-        </Container>
-    )
-}
+            <Grid container>{socialInfo}</Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </FadeIn>
+  );
+};
 
 export default Resources;
